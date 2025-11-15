edbd04ef708ffb6ab3727fe341bfaebf92f31f87
krisng <ng.phihoc123@gmail.com>
2025-11-15 09:29:51 +0700

    feat: initialize project

---
+++ src/main/java/com/ridemate/ridemate_server/RidemateServerApplication.java
@@ -0,0 +1,13 @@
+package com.ridemate.ridemate_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidemateServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidemateServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ridemate/ridemate_server/RidemateServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ridemate.ridemate_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidemateServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
